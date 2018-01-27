ccf8c2cf3620c4877f7431912f267df5d7867b01
Phong <pche3@illinois.edu>
2018-01-27 14:56:24 -0600

    Push changes to master branch

--- Script1.js
+++ Script1.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-  $("#getQuote").on("click", function() {                                  
-	$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&key=cat&format=jsonp&jsonp=?", function(json) {
-		 console.log(json.quoteText);
-		 console.log(json.quoteAuthor);
-		 $('#quote').html(json.quoteText);
-		 $('#quoteAuthor').html(json.quoteAuthor);
+  $("#getQuote").on("click", function() {   
+    $("#quote").fadeOut("slow"); 
+    $("#quoteAuthor").fadeOut("slow"); 
+    $("#buttonShake").effect("shake");
+	  $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&key=cat&format=jsonp&jsonp=?", function(json) {
+		  $('#quote').html(json.quoteText);
+     if(json.quoteAuthor != "")
+		  $('#quoteAuthor').html(json.quoteAuthor);
+     else
+      $('#quoteAuthor').html("Anonymous");
     });
-	$("body").css("background-color", "red");
+    $("#quote").fadeIn("slow"); 
+    $("#quoteAuthor").fadeIn("slow"); 
   });               
 });
-// JavaScript source code
